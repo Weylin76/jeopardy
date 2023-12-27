4aa354cb5129b6e3fa0e66d42bcd9906130ebb14
Weylin76 <Weylin76@msn.com>
2023-12-26 21:22:45 -0500

    added questions

--- kids vs adults lingo/script.js
+++ kids vs adults lingo/script.js
@@ -24,8 +24,18 @@ document.addEventListener('DOMContentLoaded', () => {
         { question: "What does 'fam' refer to?", answer: "It refers to close friends, group of friends, or sometimes used to address someone in a friendly manner." },
         { question: "What does 'slay' mean in modern lingo?", answer: "It means to do something really well, especially in a fashion or performance context." },
         { question: "If a kid says 'I'm dead' or 'I'm dying,' what do they mean?", answer: "They mean they find something extremely funny." },
-        { question: "What does 'snack' mean when referring to a person?", answer: "It means someone who looks really attractive or appealing." }
-        // ... other adult questions ...
+        { question: "What does 'snack' mean when referring to a person?", answer: "It means someone who looks really attractive or appealing." },
+        { question: "What does 'finna' mean in modern slang?", answer: "It's a contraction of 'fixing to' and means 'going to' or 'planning to.'"},
+        { question: "When a kid says 'bet,' how is it typically used?", answer: "It's used as an affirmation, similar to saying 'OK' or 'Sure.'"},
+        { question: "In youth slang, what does 'woke' refer to?", answer: "Being 'woke' means being aware of social injustices and inequalities."},
+        { question: "What does the term 'Gucci' imply in modern slang?", answer: "It means good, cool, or going well, derived from the luxury brand."},
+        { question: "If a kid says 'it's a bop,' what are they referring to?", answer: "They're saying a song is really good or catchy."},
+        { question: "What does 'thirsty' mean in contemporary slang?", answer: "It describes someone who is desperate for attention, especially romantically."},
+        { question: "What does 'receipts' mean in youth lingo?", answer: "It refers to evidence or proof, typically in the context of a dispute or gossip."},
+        { question: "In slang, what does 'lick' often refer to?", answer: "It refers to a quick theft or robbery, often opportunistic in nature."},
+        { question: "What does 'it's bussin' mean?", answer: "In modern slang, 'it's bussin' means something is really good, especially in reference to food being delicious."},
+        { question: "What does 'it slaps' mean?", answer: "In current slang, 'it slaps' is used to describe something, often music, that is exceptionally good or impressive."},
+        { question: "What does it mean to 'cop' something in current slang?", answer: "To 'cop' something means to buy or get something, especially something desirable or in demand."}
     ];
 
     const kidQuizQuestions = [
@@ -41,15 +51,25 @@ document.addEventListener('DOMContentLoaded', () => {
         { question: "What did 'booyah' express?", answer: "It was an exclamation to accompany a triumphant action or victory." },
         { question: "What did 'far out' mean in the past?", answer: "It meant something was impressive, good, or exciting." },
         { question: "What was the meaning of 'gnarly'?", answer: "It meant something was extreme, especially in a good way." },
-        { question: "What did the term 'tubular' refer to?", answer: "It meant something was excellent or cool." },
+        { question: "What does PIN stand for?", answer: "Personal Identification Number."},
         { question: "What did 'What's your damage?' mean?", answer: "It was a way of asking someone why they are being unreasonable or difficult." },
         { question: "What was the meaning of 'Word up'?", answer: "It was a phrase of agreement or a way to say 'I understand'." },
-        { question: "What did 'duh' imply when someone said it?", answer: "It expressed that something was obvious or clear." },
+        { question: "What does ATM stand for?",answer: "Automated Teller Machine."},
         { question: "What did 'funky' mean?", answer: "It referred to something that's cool, unique, or stylish." },
         { question: "What did 'hang loose' mean?", answer: "To relax or stay calm, often associated with surfer culture." },
-        { question: "What did 'no duh' mean?", answer: "It was a sarcastic way of saying 'obviously'." },
-        { question: "What did 'wig out' mean?", answer: "To become very excited or upset about something." }
-        // ... other kid questions ...
+        { question: "What did 'wig out' mean?", answer: "To become very excited or upset about something." },
+        { question: "What did 'dial-up' refer to in the past?", answer: "It referred to a type of internet connection using a telephone line, known for being slow." },
+        { question: "What does 'beeper' or 'pager' mean?", answer: "A small device for receiving text messages or call notifications." },
+        { question: "What did 'VHS' stand for?", answer: "Video Home System, a format for VCR tapes." },
+        { question: "What was a 'Walkman'?", answer: "A portable cassette player for music." },
+        { question: "What did 'rewind' mean for VHS or cassettes?", answer: "Winding tape back to the beginning or a previous point." },
+        { question: "What does 'mixtape' mean?", answer: "A compilation of favorite music tracks on a cassette." },
+        { question: "What was 'MySpace'?", answer: "An early social media platform before Facebook." },
+        { question: "What did 'fax' or 'fax machine' refer to?", answer: "A device for sending scanned documents via phone lines." },
+        { question: "What does 'brick phone' mean?", answer: "An early large and bulky mobile phone." },
+        { question: "What did 'roll down the window' originally mean?", answer: "Manually lowering a car window before automatic windows." },
+        { question: "What was a 'Discman'?", answer: "A portable CD player, similar to a Walkman but for CDs." },
+        { question: "What did 'Blockbuster night' mean?", answer: "Going to Blockbuster to rent movies for a night in, before streaming services." }
     ];
 
     let currentQuestionIndex = 0;
